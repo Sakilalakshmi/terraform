d17ac49b26bbcd441e864ed27a70e0ee0f97ba5b
sakila <sakilalakshmi95@gmail.com>
2023-10-09 21:19:00 +0530

    terraform

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -6,3 +6,23 @@ resource "aws_instance" "provisioners" {
     command = "echo ${self.private_ip} > inventory"
   }
 }
+
+resource "aws_instance" "remote" {
+    ami = "ami-03265a0778a880afb"
+    instance_type = "t2.micro"
+    security_groups = ["sg-0bb1d721bc86e4884"]
+    subnet_id = "subnet-04ed3770c5ed301a6"
+    
+    connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo systemctl start nginx",
+    ]
+  }
+}
